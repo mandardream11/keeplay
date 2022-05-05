9f6bfa84742edf065eda8a745f9c304d61dd55af
mandardream11 <mandar.vyas@dream11.com>
2022-05-05 19:04:01 +0530

    Variables

---
+++ Variables/src/AllVariables.java
@@ -0,0 +1,26 @@
+public class AllVariables {
+
+    public static void main (String[] args)
+    {
+        byte firstByte = 8;
+        short firstShort = 3200;
+        int firstInteger = 200111;
+        float firstFloat = 2.99f;
+        double firstDouble = 222222.99;
+        char firstChar = 'T';
+        String firstString = "This is the first sentence.";
+        boolean firstBoolean = true;
+
+        System.out.println("Value of first Byte is " + firstByte);
+        System.out.println("Value of first Short is " + firstShort);
+        System.out.println("Value of first Integer is " + firstInteger);
+        System.out.println("Value of first Float is " + firstFloat);
+        System.out.println("Value of first Double is " + firstDouble);
+        System.out.println("Value of first Char is " + firstChar);
+        System.out.println(firstString);
+       if(firstBoolean=true) {
+           System.out.println("TRUE");
+       }
+
+    }
+}
