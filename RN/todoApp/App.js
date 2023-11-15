7316712d4694b621e5eeeba3ade589eacdb365ba
mandardream11 <mandar.vyas@dream11.com>
2023-11-15 08:40:29 +0530

    feat: async storage support added

--- RN/todoApp/App.js
+++ RN/todoApp/App.js
@@ -1,103 +1,178 @@
-import React, { useState } from 'react';
-import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View, ScrollView, FlatList } from 'react-native';
-import Task from './components/Task'
+// App.js
 
-export default function App() {
-  const [task, setTask] = useState();
-  const [taskItems, setTaskItems] = useState([]);
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, ScrollView, Button } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-  const handleAddTask = () => {
-    Keyboard.dismiss();
-    setTaskItems([...taskItems, task])
-    setTask(null);
-  }
+const HomeScreen = ({ navigation }) => {
+  const [tasks, setTasks] = useState([]);
+  const [taskText, setTaskText] = useState('');
+  const [taskError, setTaskError] = useState('');
 
-  const completeTask = (index) => {
-    let itemsCopy = [...taskItems]
-    itemsCopy.splice(index, 1);
-    setTaskItems(itemsCopy);
-  }
+  useEffect(() => {
+    // Load tasks from AsyncStorage when the component mounts
+    loadTasks();
+  }, []);
+
+  const loadTasks = async () => {
+    try {
+      const storedTasks = await AsyncStorage.getItem('tasks');
+      if (storedTasks !== null) {
+        setTasks(JSON.parse(storedTasks));
+      }
+    } catch (error) {
+      console.error('Error loading tasks:', error);
+    }
+  };
+
+  const saveTasks = async (updatedTasks) => {
+    try {
+      await AsyncStorage.setItem('tasks', JSON.stringify(updatedTasks));
+    } catch (error) {
+      console.error('Error saving tasks:', error);
+    }
+  };
+
+  const addTask = () => {
+    if (taskText.trim() !== '') {
+      const newTasks = [...tasks, { text: taskText, id: Date.now() }];
+      setTaskError('');
+      setTasks(newTasks);
+      saveTasks(newTasks);
+      setTaskText('');
+    }
+    else {
+      setTaskError("Please enter a task.");
+    }
+  };
+
+  const removeTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+    saveTasks(updatedTasks);
+  };
 
   return (
     <View style={styles.container}>
-      { /* Today's Tasks */}
-      <ScrollView style={styles.tasksWrapper}>
-        <Text style={styles.sectionTitle}>Today's Tasks</Text>
-        <View style={styles.items}>
-          {/* Tasks should be added here */}
-          {
-            taskItems.map((item, index) => {
-              return (
-                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
-                  <Task text={item} />
-                </TouchableOpacity>
-              )
-            })
-          }
-        </View>
-      </ScrollView>
-
-      {/* Write a task */}
-      <KeyboardAvoidingView
-        behavior={Platform.OS == "ios" ? "padding" : 'height'}
-        style={styles.writeTaskWrapper}>
-        <TextInput
-          style={styles.input}
-          placeholder={'Write a task'}
-          value={task}
-          onChangeText={text => setTask(text)} />
-        <TouchableOpacity onPress={() => handleAddTask()}>
-          <View style={styles.addWrapper}>
-            <Text style={styles.addText}> + </Text>
+      <ScrollView style={styles.scrollView}>
+        {tasks.slice().reverse().map((task) => (
+          <View style={styles.taskContainer}>
+            <Text style={styles.taskText}>{task.text}</Text>
+            <TouchableOpacity key={task.id}
+              onPress={() => removeTask(task.id)} style={styles.deleteButton}>
+              <Text style={styles.deleteButtonText}>Delete</Text>
+            </TouchableOpacity>
           </View>
+        ))}
+      </ScrollView>
+      <View style={styles.inputContainer}>
+
+        <TextInput
+          style={[
+            styles.input,
+            taskError !== '' && styles.inputErrorBorder]}
+          placeholder="Add a task..."
+          onChangeText={(text) => setTaskText(text)}
+          value={taskText}
+        />
+
+        <TouchableOpacity style={styles.addButton} onPress={addTask}>
+          <Text style={styles.addButtonText}>Add</Text>
         </TouchableOpacity>
-      </KeyboardAvoidingView>
+      </View>
     </View>
+
   );
-}
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="TODO App" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E8EAED',
+    backgroundColor: '#f0f0f0',
   },
-  tasksWrapper: {
-    paddingTop: 80,
-    paddingHorizontal: 20
+  scrollView: {
+    flex: 1,
+    marginTop: 20,
+    paddingHorizontal: 20,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: 'bold'
-  },
-  items: {
-    marginTop: 30
-  },
-  writeTaskWrapper: {
-    position: "absolute",
-    bottom: 60,
-    width: '100%',
+  taskContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center'
+    alignItems: 'center', // Center children vertically
+    justifyContent: 'space-between',
+    backgroundColor: '#ffffff',
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 10,
+  },
+  taskText: {
+    flex: 1,
+    fontSize: 16,
+  },
+  deleteButton: {
+    backgroundColor: '#61dafb',
+    borderRadius: 5,
+    marginLeft: 10
+  },
+  deleteButtonText: {
+    color: '#ffffff',
+    fontSize: 16,
+    padding: 5
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 20,
+    borderTopWidth: 1,
+    borderTopColor: '#cccccc',
+    backgroundColor: '#ffffff',
+  },
+  inputErrorBorder: {
+    borderColor: 'red'
+  },
+  inputWrapper: {
+    flex: 1,
   },
   input: {
-    paddingVertical: 15,
-    paddingHorizontal: 15,
-    backgroundColor: '#FFF',
-    borderRadius: 60,
-    width: 250,
-    borderColor: '#C0C0C0',
-    borderWidth: 1
+    flex: 1,
+    height: 40,
+    marginRight: 10,
+    paddingLeft: 10,
+    borderColor: '#cccccc',
+    borderWidth: 1,
+    borderRadius: 5,
   },
-  addWrapper: {
-    width: 60,
-    height: 60,
-    backgroundColor: '#FFF',
-    borderRadius: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#C0C0C0',
-    borderWidth: 1
+  errorContainer: {
+    marginTop: 5,
   },
-  addText: {},
+  inlineErrorText: {
+    color: 'red',
+    fontSize: 10
+  },
+  addButton: {
+    backgroundColor: '#61dafb',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  addButtonText: {
+    color: '#ffffff',
+    fontSize: 16,
+  }
 });
+
+export default App;
